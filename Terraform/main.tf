085fb89f74b7a0148806db12f4f1c2024bbea050
MoonishaaA <amoonishaa@gmail.com>
2025-08-07 15:54:35 +0530

    update public and private subnet

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,22 @@
 resource "aws_vpc" "mvpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyVPC"
+  }
 }
 
 resource "aws_subnet" "publicsubnet" {
     vpc_id = aws_vpc.mvpc.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+        Name = "MyPublicSubnet"
+    }
 }
 
 resource "aws_subnet" "privatesubnet" {
     vpc_id = aws_vpc.mvpc.id
-  
+    cidr_block = "10.0.2.0/24"
+    tags = {
+        Name = "MyPrivateSubnet"
+    }
 }
