31a9e4c9cdd4f5e9e7b94430eebf5ef2f45722c2
MoonishaaA <131514438+MoonishaaA@users.noreply.github.com>
2025-08-08 13:48:25 +0530

    Update main.tf

--- Terraform-aws/main.tf
+++ Terraform-aws/main.tf
@@ -24,9 +24,7 @@ resource "aws_subnet" "privatesubnet" {
 resource "aws_eks_cluster" "private_eks" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
-
   version = "1.21"
-
   vpc_config {
     subnet_ids = [
       aws_subnet.publicsubnet.id,
