9b3477af2fe7d1f8bc0d9805c993bd05447d37db
MoonishaaA <amoonishaa@gmail.com>
2025-08-11 21:01:33 +0530

    keypair

--- Terraform-aws/main.tf
+++ Terraform-aws/main.tf
@@ -1,13 +1,15 @@
-
-# VPC & Subnets
 resource "aws_vpc" "mvpc" {
   cidr_block = "10.0.0.0/16"
-  tags = { Name = "MyVPC" }
+  tags = {
+    Name = "MyVPC"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.mvpc.id
-  tags   = { Name = "MyInternetGateway" }
+  tags = {
+    Name = "MyIGW"
+  }
 }
 
 resource "aws_subnet" "publicsubnet" {
@@ -15,108 +17,82 @@ resource "aws_subnet" "publicsubnet" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
-  tags = { Name = "MyPublicSubnet" }
+  tags = {
+    Name = "MyPublicSubnet"
+  }
 }
 
 resource "aws_subnet" "privatesubnet" {
   vpc_id            = aws_vpc.mvpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1a"
-  tags = { Name = "MyPrivateSubnet" }
+  tags = {
+    Name = "MyPrivateSubnet"
+  }
 }
 
-resource "aws_route_table" "public" {
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.mvpc.id
-  tags   = { Name = "MyPublicRouteTable" }
+  tags = {
+    Name = "PublicRouteTable"
+  }
 }
 
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-resource "aws_route_table_association" "public_subnet" {
+resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.publicsubnet.id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-# Security Groups
-resource "aws_security_group" "bastion_sg" {
-  vpc_id = aws_vpc.mvpc.id
-  name   = "bastion-sg"
-
-  ingress {
-    description = "SSH from my IP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["YOUR_PUBLIC_IP/32"] # Replace with your IP
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "eks_sg" {
-  vpc_id = aws_vpc.mvpc.id
-  name   = "eks-sg"
-
-  ingress {
-    description     = "Allow 443 from bastion"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]
-  }
-
-  ingress {
-    description = "Allow node-to-node"
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Bastion Host
-resource "aws_instance" "bastion" {
-  ami                    = "ami-0c02fb55956c7d316" 
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.publicsubnet.id
-  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-  key_name               = "pro.pem" 
-  tags = { Name = "BastionHost" }
-}
-
-# IAM Roles for EKS
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eksClusterRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_eks_cluster" "private_eks" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.29"
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.privatesubnet.id 
+    ]
+    endpoint_private_access = true
+    endpoint_public_access  = false
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSServicePolicy
+  ]
 }
 
 resource "aws_iam_role" "eks_node_role" {
@@ -124,11 +100,15 @@ resource "aws_iam_role" "eks_node_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
   })
 }
 
@@ -142,34 +122,41 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   role       = aws_iam_role.eks_node_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "eks_registry_policy" {
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node_role.name
-}
-
-# EKS Cluster & Node Group
-resource "aws_eks_cluster" "private_eks" {
-  name     = "my-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = "1.29"
-
-  vpc_config {
-    subnet_ids              = [aws_subnet.publicsubnet.id, aws_subnet.privatesubnet.id]
-    endpoint_private_access = true
-    endpoint_public_access  = false
-    security_group_ids      = [aws_security_group.eks_sg.id]
-  }
 }
 
 resource "aws_eks_node_group" "private_nodes" {
   cluster_name    = aws_eks_cluster.private_eks.name
   node_group_name = "private-nodes"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.privatesubnet.id]
-  instance_types  = ["t3.small"]
+  subnet_ids      = [aws_subnet.privatesubnet.id] 
+
   scaling_config {
     desired_size = 2
     max_size     = 3
     min_size     = 1
   }
+
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_readonly
+  ]
+}
+
+resource "aws_db_instance" "rds_instance" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  db_name              = "mydb"
+  username             = "admin"
+  password             = "12387654"
+  storage_type         = "gp2"
+  skip_final_snapshot  = true
 }
