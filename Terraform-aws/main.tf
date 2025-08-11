65c5dfd4c4297bfff333342e9da67ac13f391a0d
MoonishaaA <amoonishaa@gmail.com>
2025-08-11 20:23:17 +0530

    keypair

--- Terraform-aws/main.tf
+++ Terraform-aws/main.tf
@@ -92,12 +92,11 @@ resource "aws_security_group" "eks_sg" {
 
 # Bastion Host
 resource "aws_instance" "bastion" {
-  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  ami                    = "ami-0c02fb55956c7d316" 
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.publicsubnet.id
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-  key_name               = "my-keypair" # Replace with your EC2 key pair name
-
+  key_name               = "pro.pem" 
   tags = { Name = "BastionHost" }
 }
 
