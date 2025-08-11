cfe73afef8a6647f11882a269229d094828f64ed
MoonishaaA <amoonishaa@gmail.com>
2025-08-11 15:22:13 +0530

    igw

--- Terraform-aws/main.tf
+++ Terraform-aws/main.tf
@@ -5,48 +5,50 @@ resource "aws_vpc" "mvpc" {
   }
 }
 
-resource "aws_subnet" "publicsubnet" {
-    vpc_id = aws_vpc.mvpc.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "us-east-1b"
-    tags = {
-        Name = "MyPublicSubnet"
-    }
-}
-
-resource "aws_subnet" "privatesubnet" {
-    vpc_id = aws_vpc.mvpc.id
-    cidr_block = "10.0.2.0/24"
-    availability_zone  = "us-east-1a"
-    tags = {
-        Name = "MyPrivateSubnet"
-    }
-}
-
-resource "aws_eks_cluster" "private_eks" {
-  name     = "my-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-  version = "1.29"
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.publicsubnet.id,
-      aws_subnet.privatesubnet.id
-    ]
-    endpoint_private_access = true
-    endpoint_public_access = false
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.mvpc.id
+  tags = {
+    Name = "MyIGW"
   }
 }
 
-resource "aws_db_instance" "rds_instance" {
-  allocated_storage = 20
-  engine = "mysql"
-  engine_version = "8.0"
-  instance_class = "db.t3.micro"
-  db_name = "mydb"
-  password = "12387654"
-  username = "admin"
-  storage_type = "gp2"
+resource "aws_subnet" "publicsubnet" {
+  vpc_id                  = aws_vpc.mvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MyPublicSubnet"
+  }
 }
+
+resource "aws_subnet" "privatesubnet" {
+  vpc_id            = aws_vpc.mvpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "MyPrivateSubnet"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.mvpc.id
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.publicsubnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eksClusterRole"
 
@@ -62,7 +64,6 @@ resource "aws_iam_role" "eks_cluster_role" {
       }
     ]
   })
-
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
@@ -73,4 +74,47 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_eks_cluster" "private_eks" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.29"
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.publicsubnet.id,
+      aws_subnet.privatesubnet.id
+    ]
+    endpoint_private_access = true
+    endpoint_public_access  = false
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSServicePolicy
+  ]
+}
+resource "aws_db_instance" "rds_instance" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  db_name              = "mydb"
+  username             = "admin"
+  password             = "12387654"
+  storage_type         = "gp2"
+  skip_final_snapshot  = true
+}
+
+resource "aws_instance" "bastion" {
+  ami                         = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.publicsubnet.id
+  associate_public_ip_address = true
+  key_name                    = "my-keypair" # change to your existing key pair
+
+  tags = {
+    Name = "BastionHost"
+  }
 }
