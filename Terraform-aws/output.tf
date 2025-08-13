720c07ecf519df19b29e0356802508695131441f
MoonishaaA <amoonishaa@gmail.com>
2025-08-13 12:51:51 +0530

    eks

--- Terraform-aws/output.tf
+++ Terraform-aws/output.tf
@@ -1,19 +1,26 @@
-output "mvpc" {
-    value= aws_vpc.mvpc.id 
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
 }
 
-output "publicsubnet" {
-    value=aws_subnet.publicsubnet.id 
+output "eks_cluster_name" {
+  value = aws_eks_cluster.private_eks.name
 }
 
-output "privatesubnet" {
-    value=aws_subnet.privatesubnet.id 
+output "kubeconfig" {
+  value = <<EOT
+aws eks --region us-east-1 update-kubeconfig --name ${aws_eks_cluster.private_eks.name}
+EOT
 }
 
-output "cluster" {
-    value = aws_eks_cluster.private_eks.name
+output "db_endpoint" {
+  value = aws_db_instance.wordpress.address
 }
 
-output "db_instance" {
-    value = aws_db_instance.rds_instance.id
+output "db_username" {
+  value = aws_db_instance.wordpress.username
+}
+
+output "db_password" {
+  value     = var.db_password
+  sensitive = true
 }
--- Terraform-aws/variables.tf
+++ Terraform-aws/variables.tf
@@ -1,3 +1,9 @@
 variable "region" {
     default = "us-east-1"
   }
+
+  variable "db_password" {
+    description = "The database password"
+    type        = string
+    sensitive   = true
+  }
