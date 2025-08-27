e148614193bfce02a4e0fb40c8ce43fa2f53245b
MoonishaaA <amoonishaa@gmail.com>
2025-08-27 13:33:47 +0000

    updated

--- Terraform-gcp/backend.tf
+++ Terraform-gcp/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "gcs" {
     bucket      = "mminiproject-bucket"   # Replace with your GCS bucket
-    prefix      = "terraform/state"             # Folder inside the bucket
-    project     = "my-project-learning-464616"           # Replace with your GCP project
+    prefix      = "terraform/state"             #  Replace with your GCP project
   }
 }
